39b226a6413c9b8cfff134ff591fd4288ee1cae4
Emmanuel Alves <manel.pb@gmail.com>
2018-01-13 15:01:48 -0500

    fix linting issues

--- src/plugin.js
+++ src/plugin.js
@@ -1,150 +1,142 @@
 import videojs from 'video.js';
 
 class QualitySelector {
-	constructor(player) {
-		this.player = player;
-		this.sources = [];
-		this.callback = undefined;
-		this.containerDropdownElement = undefined;
-		this.defaults = {};
-	}
+  constructor(player) {
+    this.player = player;
+    this.sources = [];
+    this.callback = undefined;
+    this.containerDropdownElement = undefined;
+    this.defaults = {};
+  }
 
-  /**
-   * event on selected the quality
-   */
+	/**
+	 * event on selected the quality
+	 */
   onQualitySelect(quality) {
-		if(this.callback) {
+    if (this.callback) {
       this.callback(quality);
-		};
+    }
 
-		if(this.sources) {
-			// tries to find the source with this quality
-			let source = this.sources.find(source => source.format == quality.code);
+    if (this.sources) {
+      // tries to find the source with this quality
+      let source = this.sources.find(ss => ss.format === quality.code);
 
-			if(source) {
+      if (source) {
         this.player.src({ src: source.src, type: source.type });
 
-        this.player.on("loadedmetadata", () => {
-					this.player.play();
+        this.player.on('loadedmetadata', () => {
+          this.player.play();
 
-					Array.from(this.containerDropdownElement.firstChild.childNodes).forEach(ele => {
-						if (ele.dataset.code === quality.code) {
-							ele.setAttribute("class", "current");
-						} else {
-							ele.removeAttribute("class");
-						}
-					});
-				});
-			}
+          Array.from(this.containerDropdownElement.firstChild.childNodes).forEach(ele => {
+            if (ele.dataset.code === quality.code) {
+              ele.setAttribute('class', 'current');
+            } else {
+              ele.removeAttribute('class');
+            }
+          });
+        });
+      }
 
-			const qualitySelector = document.getElementById(this.player.id_).getElementsByClassName('vjs-brand-quality-link');
+      const player = document.getElementById(this.player.id_);
+      const qualitySelector = player.getElementsByClassName('vjs-brand-quality-link');
 
-			if (qualitySelector && qualitySelector.length > 0) {
-				qualitySelector[0].innerText = quality.name;
-			}
-		}
+      if (qualitySelector && qualitySelector.length > 0) {
+        qualitySelector[0].innerText = quality.name;
+      }
+    }
 
     this.onToggleDropdown();
-	};
+  }
 
-  /**
-   * show or hide the dropdown
-   */
-	onToggleDropdown() {
-		if(this.containerDropdownElement.className.indexOf("show") == -1) {
-			this.containerDropdownElement.className += " show";
-		} else {
-			this.containerDropdownElement.className = this.containerDropdownElement.className.replace(" show", "");
-		}
-	};
+	/**
+	 * show or hide the dropdown
+	 */
+  onToggleDropdown() {
+    if (this.containerDropdownElement.className.indexOf('show') === -1) {
+      this.containerDropdownElement.className += ' show';
+    } else {
+      const className = this.containerDropdownElement.className.replace(' show', '');
 
-  /**
-   * Function to invoke when the player is ready.
-   *
-   * This is a great place for your plugin to initialize itself. When this
-   * function is called, the player will have its DOM and child components
-   * in place.
-   *
-   * @function onPlayerReady
-   * @param    {Player} player
-   * @param    {Object} [options={}]
-   */
+      this.containerDropdownElement.className = className;
+    }
+  }
+
+	/**
+	 * Function to invoke when the player is ready.
+	 *
+	 * This is a great place for your plugin to initialize itself. When this
+	 * function is called, the player will have its DOM and child components
+	 * in place.
+	 *
+	 * @function onPlayerReady
+	 * @param    {Player} player
+	 * @param    {Object} [options={}]
+	 */
   onPlayerReady(options) {
-		this.containerDropdownElement = document.createElement("div");
-    this.containerDropdownElement.className = "vjs-quality-dropdown";
+    this.containerDropdownElement = document.createElement('div');
+    this.containerDropdownElement.className = 'vjs-quality-dropdown';
 
-		let containerElement = document.createElement("div");
-		containerElement.className = "vjs-quality-container";
+    let containerElement = document.createElement('div');
 
-		let buttonElement = document.createElement("button");
-		buttonElement.className = "vjs-brand-quality-link";
-		buttonElement.onclick = (event) => this.onToggleDropdown(event);
-		buttonElement.innerText = options.text || "Quality";
+    containerElement.className = 'vjs-quality-container';
 
-		let ulElement = document.createElement("ul");
+    let buttonElement = document.createElement('button');
 
-		if(!options.formats) {
-			options.formats = [{ code: 'auto', name: 'Auto' }];
-		}
+    buttonElement.className = 'vjs-brand-quality-link';
+    buttonElement.onclick = (event) => this.onToggleDropdown(event);
+    buttonElement.innerText = options.text || 'Quality';
 
-		if(options.onFormatSelected) {
+    let ulElement = document.createElement('ul');
+
+    if (!options.formats) {
+      options.formats = [{ code: 'auto', name: 'Auto' }];
+    }
+
+    if (options.onFormatSelected) {
       this.callback = options.onFormatSelected;
-		}
+    }
 
-		if(options.sources) {
+    if (options.sources) {
       this.sources = options.sources;
-		}
+    }
 
-		options.formats.map((format) => {
-			let liElement = document.createElement("li");
-			liElement.dataset.code = format.code;
+    options.formats.map((format) => {
+      let liElement = document.createElement('li');
 
-			let linkElement = document.createElement("a");
-			linkElement.innerText = format.name;
-			linkElement.setAttribute("href", "#");
-			linkElement.addEventListener("click", (event) => {
-				event.preventDefault();
-				this.onQualitySelect(format);
-			});
+      liElement.dataset.code = format.code;
 
-			liElement.appendChild(linkElement);
-			ulElement.appendChild(liElement);
-		});
+      let linkElement = document.createElement('a');
+
+      linkElement.innerText = format.name;
+      linkElement.setAttribute('href', '#');
+      linkElement.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.onQualitySelect(format);
+      });
+
+      liElement.appendChild(linkElement);
+      ulElement.appendChild(liElement);
+    });
 
     this.containerDropdownElement.appendChild(ulElement);
-		containerElement.appendChild(this.containerDropdownElement);
-		containerElement.appendChild(buttonElement);
+    containerElement.appendChild(this.containerDropdownElement);
+    containerElement.appendChild(buttonElement);
 
-    this.player.controlBar.el().insertBefore(containerElement, this.player.controlBar.fullscreenToggle.el());
+    const fullScreenToggle = this.player.controlBar.fullscreenToggle.el();
+
+    this.player.controlBar.el().insertBefore(containerElement, fullScreenToggle);
 
     this.player.addClass('vjs-qualityselector');
-
-		/*this.player.on("loadedmetadata", () => {
-
-			console.log(options);
-
-
-			const removeHttpPrefix = url => url.replace(/(^\w+:|^)\/\//, '');
-			const current_src = removeHttpPrefix(this.player.currentSrc());
-			const current = this.sources.find(ele => removeHttpPrefix(ele.src) === current_src);
-			Array.from(this.containerDropdownElement.firstChild.childNodes).forEach(ele => {
-				if (ele.dataset.code === current.format) {
-					ele.setAttribute("class", "current");
-				} else {
-					ele.removeAttribute("class");
-				}
-			});
-		});*/
-  };
+  }
 }
 
 /**
  * A video.js plugin.
  *
  * In the plugin function, the value of `this` is a video.js `Player`
- * instance. You cannot rely on the player being in a "ready" state here,
+ * instance. You cannot rely on the player being in a 'ready' state here,
  * depending on how the plugin is invoked. This may or may not be important
- * to you; if not, remove the wait for "ready"!
+ * to you; if not, remove the wait for 'ready'!
  *
  * @function qualityselector
  * @param    {Object} [options={}]
@@ -152,14 +144,15 @@ class QualitySelector {
  */
 const qualityselector = function(options) {
   this.ready(() => {
-  	const player = this;
-  	let qualityControl = new QualitySelector(player);
+    let qualityControl = new QualitySelector(this);
+
     qualityControl.onPlayerReady(videojs.mergeOptions(qualityControl.defaults, options));
   });
 };
 
 // Register the plugin with video.js.
 const registerPlugin = videojs.registerPlugin || videojs.plugin;
+
 registerPlugin('qualityselector', qualityselector);
 
 // Include the version number.
